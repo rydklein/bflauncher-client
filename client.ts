9e61f6939132bed2fe30b243aacad70084a1c6b0
Ryder Klein <Ryder679@live.com>
2021-06-14 20:00:57 -0400

    Refactored Origin Interface (again).

--- client.ts
+++ client.ts
@@ -38,107 +38,188 @@ console.log(`Seeder Control Server set to ${controlServer}`);
 // Global Variables
 let battlelog:Battlelog;
 let serverInterface:ServerInterface;
-let originChildProcess:  child_process.ChildProcess;
-let originLaunched: boolean;
-let signingIn = false;
-
+let originInterface:OriginInterface;
 // #region Origin
-async function originSignIn():Promise<void> {
-    if (signingIn) return;
-    signingIn = true;
-    originLaunched = false;
-    // Find Origin.exe's PID and kill it.
-    // If this is the first launch, and originChildProcess doesn't exist yet:
-    if (!originChildProcess) {
-        // findProcess is very inefficient/slow so we try and avoid it if at all possible.
-        const originProcess: any = await findProcess("Origin.exe");
-        if (originProcess) {
-            process.kill(originProcess.pid);
+class OriginInterface extends EventEmitter {
+    // If Origin is running AND accessable to the debugger.
+    public originRunning = false;
+    // If Origin is signed in to the proper account.
+    public signedIn = false;
+    // Self-explanitory
+    public hasBF4 = false;
+    public hasBF1 = false;
+    // Private
+    private email:string;
+    private password:string;
+    private originInstance!:child_process.ChildProcess;
+    private originDebugger;
+    private watchdogTimer;
+    constructor(email, password) {
+        super();
+        this.email = email;
+        this.password = password;
+        this.init();
+    }
+    private async init() {
+        await this.restartOrigin();
+        if (!(await this.startDebugger())) {
+            throw new Error("Error initializing Origin.");
         }
-    } else {
-        originChildProcess.kill();
-    }
-    console.log("Relaunching Origin...");
-    await wait(250);
-    // Launch it again!
-    originChildProcess = child_process.spawn(`${process.env["ProgramFiles(x86)"]}\\Origin\\Origin.exe`, ["--remote-debugging-port=8081"]);
-    console.log("Relaunched Origin. Waiting for Origin to finish starting...");
-    await wait(5000);
-    // Initialize our CRI client.
-    const debugOptions: any = {
-        port: "8081",
-    };
-    let tabs;
-    while(!tabs){
-        try {
-            tabs = await CRI.List(debugOptions);
-        } catch {
-            await wait(1000);
-            continue;
+        // Get array of installed games
+        // Shitty workaround due to old version of Debug Tools
+        await this.originDebugger.Runtime.evaluate({"expression":"window.Origin.client.games.requestGamesStatus().then((out) => {window.currentGames = out;})"});
+        let installedGames;
+        while(!installedGames) {
+            await wait(100);
+            installedGames = JSON.parse((await this.originDebugger.Runtime.evaluate({"expression":"JSON.stringify(window.currentGames.updatedGames)"})).result.value);
         }
+        await this.originDebugger.close();
+        for (const game of installedGames) {
+            if (OriginInterface.bf1OfferIds.includes(game.productId) && game.playable) this.hasBF1 = true;
+            if (OriginInterface.bf4OfferIds.includes(game.productId) && game.playable) this.hasBF4 = true;
+        }
+        this.emit("ready");
+        this.watchdogTimer = setInterval(this.originWatchdog, 30000);
     }
-    let loginTab;
-    while (!loginTab) {
-        // Don't lock up the process
-        await wait(500);
-        // List our tabs again
-        tabs = await CRI.List(debugOptions);
-        // Look to see if we're already signed in. if so, break.
-        if (tabs.find(element => element.url.includes("origin.com/usa"))) break;
-        // Look for our login tab.
-        loginTab = tabs.find(element => element.url.includes("signin.ea.com"));
-    }
-    // If we need to login
-    if (loginTab) {
-        debugOptions.protocol = protocol;
-        debugOptions.target = loginTab.webSocketDebuggerUrl;
-        const client = await CRI(debugOptions);
-        await client.Runtime.enable();
-        console.log("Signing into Origin.");
-        // TODO: Add error handling. Too much work at the moment.
-        await evalCode(client, `document.getElementById('email').value = '${config.email}`);
-        await evalCode(client, `document.getElementById('password').value = '${config.password}'`);
-        await evalCode(client, "document.getElementById('rememberMe').checked = true");
-        await wait(10);
-        await evalCode(client, "document.getElementById('logInBtn').click()");
-        await client.close();  
-        console.log("Now signed into Origin.");
-    } else {
-        console.log("Already signed into Origin.");
-    }
-    originLaunched = true;
-    signingIn = false;
-    return;
-}
-async function originWatchdog() {
-    // If we're currently launching Origin, return.
-    if (!originLaunched) return;
-    let success = true;
-    try {
-        // Check if Origin's frozen using the magic link.
-        await fetch("http://127.0.0.1:3215/ping", { timeout: 1000 });
-    } catch (e) {
-        success = false;
-    }
-    if (!success) {
-        console.log("Origin hung. Relaunching...");
-        await originSignIn();
+    // Bind this to function so that when called from a timer, we can still refer to this.
+    private restartOrigin = async ():Promise<void> => {
+        if (!this.originInstance) {
+            // findProcess is very inefficient/slow so we try and avoid it if at all possible.
+            const originProcess: any = await OriginInterface.findProcess("Origin.exe");
+            if (originProcess) {
+                process.kill(originProcess.pid);
+            }
+        } else {
+            this.originInstance.kill();
+        }
+        this.originRunning = false;
+        // Launch it again!
+        this.originInstance = child_process.spawn(`${process.env["ProgramFiles(x86)"]}\\Origin\\Origin.exe`, ["--remote-debugging-port=8081"]);
+        this.originRunning = true;
         return;
     }
+    private startDebugger = async ():Promise<boolean> => {
+        const debugOptions: any = {
+            port: "8081",
+        };
+        const debugOptionsProtocol: any = {
+            port: "8081",
+            protocol: protocol,
+        };
+        let tabs;
+        // Wait for debugger to connect.
+        while(!tabs){
+            try {
+                tabs = await CRI.List(debugOptions);
+            } catch {
+                // Don't lock up the process
+                await wait(500);
+                continue;
+            }
+        }
+        let loginTab;
+        let homePage;
+        while (!(loginTab || homePage)) {
+            // Don't lock up the process
+            await wait(500);
+            // List our tabs again
+            tabs = await CRI.List(debugOptions);
+            // Look to see if we're already signed in. if so, break.
+            // Look for our login tab &/or home page.
+            // TODO: Find a way to find these tabs that doesn't depend on href, at least the one with USA in it.
+            homePage = tabs.find(element => element.url.includes("origin.com/usa"));
+            loginTab = tabs.find(element => element.url.includes("signin.ea.com"));
+        }
+        if (homePage) this.signedIn = true;
+        if (loginTab) this.signedIn = false;
+        // If we need to login
+        if (loginTab) {
+            debugOptionsProtocol.target = loginTab.webSocketDebuggerUrl;
+            const client = await CRI(debugOptionsProtocol);
+            await client.Runtime.enable();
+            await client.Page.enable();
+            console.log("Signing into Origin.");
+            // TODO: Add error handling. Too much work at the moment.
+            await evalCode(client, `document.getElementById('email').value = '${this.email}'`);
+            await evalCode(client, `document.getElementById('password').value = '${this.password}'`);
+            await evalCode(client, "document.getElementById('rememberMe').checked = true");
+            await wait(10);
+            // Resolve promise if the load event fires.
+            const loggedInPromise = new Promise<boolean>((resolve) => {
+                client.on("Page.loadEventFired", () => {
+                    resolve(true);
+                });
+            });
+            // If it's equal to 0, it worked. If not, uh oh.
+            evalCode(client, "document.getElementById('logInBtn').click()");
+            const loginSuccess = await Promise.race([loggedInPromise, wait(5000)]);
+            await client.close();
+            if (loginSuccess) {
+                this.signedIn = true;
+            } else {
+                this.signedIn = false;
+                return false;
+            }
+            delete debugOptionsProtocol.target;
+        }
+        while (!homePage) {
+            await wait(250);
+            tabs = await CRI.List(debugOptions);
+            // Homepage has to exist since we already signed in successfully.
+            homePage = tabs.find(element => element.url.includes("origin.com/usa"));
+        }
+        debugOptionsProtocol.target = homePage.webSocketDebuggerUrl;
+        this.originDebugger = await CRI(debugOptionsProtocol);
+        await this.originDebugger.Page.enable();
+        await Promise.race([this.originDebugger.Page.loadEventFired(), wait(5000)]);
+        await this.originDebugger.Runtime.enable();
+        return true;
+    }
+    // Bind this so that we can call the function from a timer without breaking things.
+     private originWatchdog = (async () => {
+         // If we're currently launching/initializing Origin, return.
+         if (!this.signedIn) return;
+         let success = true;
+         try {
+             // Check if Origin's frozen using the magic link.
+             await fetch("http://127.0.0.1:3215/ping", { timeout: 1000 });
+         } catch (e) {
+             success = false;
+         }
+         if (!success) {
+             console.log("Origin hung. Relaunching...");
+             await this.restartOrigin();
+             await this.startDebugger();
+             return;
+         }
+     }).bind(this);
+     // Promisify ps.lookup
+     static findProcess(name: string):Promise<Record<string, unknown>> {
+         return new Promise(function (resolve, reject) {
+             ps.lookup({ command: name }, function (err, results) {
+                 if (err) {
+                     reject(err);
+                 }
+                 resolve(results[0]);
+             });
+         });
+     }
+    static bf1OfferIds = ["Origin.OFR.50.0001662", "Origin.OFR.50.0001385", "Origin.OFR.50.0000557"];
+    static bf4OfferIds = ["OFB-EAST:109552316", "OFB-EAST:109549060", "OFB-EAST:109546867", "OFB-EAST:109552312"];
 }
 // #endregion
 // #region Battlelog
 class Battlelog extends EventEmitter {
-    gameState: GameState;
-    signedIn = false;
-    browser!: puppeteer.Browser;
-    blPage!: puppeteer.Page;
+    public gameState: GameState;
+    public signedIn = false;
+    private browser!: puppeteer.Browser;
+    private blPage!: puppeteer.Page;
     constructor() {
         super();
         this.gameState = GameState.INIT;
         this.init();
     }
-    async init(): Promise<void> {
+    private async init(): Promise<void> {
         const downloadChromeOptions = {
             "revision": "884014",
             "installPath":path.join(process.cwd(), ".local-chromium"),
@@ -180,7 +261,7 @@ class Battlelog extends EventEmitter {
         });
         this.emit("readyToLogin");
     }
-    async login(email: string, password: string): Promise<boolean> {
+    public async login(email: string, password: string): Promise<boolean> {
         await this.blPage.goto("https://accounts.ea.com/connect/auth?locale=en_US&state=bf4&redirect_uri=https%3A%2F%2Fbattlelog.battlefield.com%2Fsso%2F%3Ftokentype%3Dcode&response_type=code&client_id=battlelog&display=web%2Flogin");
         await this.blPage.type("#email", email);
         await this.blPage.type("#password", password);
@@ -199,7 +280,7 @@ class Battlelog extends EventEmitter {
             });
         });
     }
-    async joinServer(guid:string): Promise<boolean> {
+    public async joinServer(guid:string): Promise<boolean> {
         const jsGoto = await this.blPage.goto(`https://battlelog.battlefield.com/bf4/servers/show/pc/${guid}`);
         if (!jsGoto.ok()) {
             switch(jsGoto.status()) {
@@ -216,7 +297,7 @@ class Battlelog extends EventEmitter {
         await this.blPage.click("[data-track=\"serverbrowser.server.join\"]");
         return true;
     }
-    async leaveServer(): Promise<boolean> {
+    public async leaveServer(): Promise<boolean> {
         try {
             await this.blPage.evaluate("gamemanager.cancelGameLaunch(gamemanager.launcherState.currentGame);");
         } catch {
@@ -318,22 +399,6 @@ type ServerData = {
 async function evalCode(client, code:string) {
     return (await client.Runtime.evaluate({ "expression": code })).result.value;
 }
-function wait(delay) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, delay);
-    });
-}
-// Promisify ps.lookup
-function findProcess(name: string) {
-    return new Promise(function (resolve, reject) {
-        ps.lookup({ command: name }, function (err, results) {
-            if (err) {
-                reject(err);
-            }
-            resolve(results[0]);
-        });
-    });
-}
 async function antiIdle() {
     if (battlelog.gameState !== GameState.IN_GAME) return;
     const windows:Array<nodeWindows.Window> = nodeWindows.windowManager.getWindows();
@@ -352,24 +417,29 @@ async function antiIdle() {
     await wait(1000);
     bf4Window.minimize();
 }
+function wait(delay) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, delay);
+    });
+}
+function waitForEvent(emitter:EventEmitter, eventName:string) {
+    return new Promise((res) => {
+        emitter.once(eventName, res);
+    });
+}
 // #endregion
 async function main() {
-    await originSignIn();
-    setInterval(originWatchdog, 30000);
+    originInterface = new OriginInterface(config.email, config.password);
+    await waitForEvent(originInterface, "ready");
     console.log("Initializing Battlelog.");
     battlelog = new Battlelog();
-    battlelog.on("readyToLogin", async () => {
-        // Keep getting gateway timeouts. This way, it tries until it logs in.
-        let loggedIn = false;
-        while(!loggedIn) {
-            loggedIn = await battlelog.login(config.email, config.password);
-        }
-    });
-    battlelog.on("loggedIn", async () => {
-        console.log("Battlelog Ready.");
-        serverInterface = new ServerInterface(controlServer!, authToken!, battlelog );
-        setInterval(antiIdle, 120000);
-    });
+    await waitForEvent(battlelog, "readyToLogin");
+    // Keep getting gateway timeouts. This way, it tries until it logs in.
+    let loggedIn = false;
+    while(!loggedIn) {
+        loggedIn = await battlelog.login(config.email, config.password);
+    }
+    serverInterface = new ServerInterface(controlServer!, authToken!, battlelog );
     battlelog.on("gameStateChange", async (launcherEvent: LauncherEvent, lastGameState: GameState) => {
         console.log(`${GameState[lastGameState]} -> ${GameState[battlelog.gameState]}`);
     });
